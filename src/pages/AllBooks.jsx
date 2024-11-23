febe9724c76b8a1fc18d9c88d98e2ef59b374bfc
FAHIM-33 <shahriyerfahim2012@gmail.com>
2024-11-24 01:31:07 +0600

    useless

--- src/pages/AllBooks.jsx
+++ src/pages/AllBooks.jsx
@@ -7,7 +7,7 @@ import { useEffect, useState } from "react";
 import { useGetAllBooksQuery } from "../redux/query/booksApi";
 
 
-//no use for
+//no use for this
 const AllBooks = () => {
     // const queryClient = useQueryClient()
     const [isAvailableOnly, setIsAvailableOnly] = useState(false)
